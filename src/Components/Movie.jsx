3acf73ca7ed5cac5b13cbb5d4d9702ae20dd7878
Izaz Ahamed Mallick <izazahamedmallick98@gmail.com>
2024-10-02 11:34:29 +0530

    error message

--- src/Components/Movie.jsx
+++ src/Components/Movie.jsx
@@ -8,76 +8,85 @@ import Loading from "./Loading";
 import InfiniteScroll from "react-infinite-scroll-component";
 
 const Movie = () => {
-  document.title = "SPMovies || Movies";
-  const navigate = useNavigate();
-  const [movieData, setMovieData] = useState("now_playing");
+    document.title = "SPMovies || Movies";
+    const navigate = useNavigate();
+    const [movieData, setMovieData] = useState("now_playing");
 
-  const [movie, setCardMovieData] = useState([]);
-  const [page, setPage] = useState(1);
+    const [movie, setCardMovieData] = useState([]);
+    const [page, setPage] = useState(1);
 
-  const getMoviePageData = async () => {
-    try {
-      const { data } = await axios.get(`/movie/${movieData}?page=${page}`);
+    const getMoviePageData = async () => {
+        try {
+            const { data } = await axios.get(
+                `/movie/${movieData}?page=${page}`
+            );
 
-      setCardMovieData((prev) => [...prev, ...data.results]);
-      setPage(page + 1);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+            setCardMovieData((prev) => [...prev, ...data.results]);
+            setPage(page + 1);
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
 
-  const refreshHandler = () => {
-    if (movie.length === 0) {
-      getMoviePageData();
-    } else {
-      setCardMovieData([]);
-      setPage(page + 1);
-      getMoviePageData();
-    }
-  };
+    const refreshHandler = () => {
+        if (movie.length === 0) {
+            getMoviePageData();
+        } else {
+            setCardMovieData([]);
+            setPage(page + 1);
+            getMoviePageData();
+        }
+    };
 
-  useEffect(() => {
-    refreshHandler();
-  }, [movieData]);
-  return movie.length > 0 ? (
-    <>
-      <div className="w-full h-full">
-        <div className=" fixed top-0 bg-[#1F1E24] z-[100] w-full flex items-center justify-between px-[3%] ">
-          <h1 className="text-2xl font-semibold text-zinc-600 w-[15%] ">
-            <i
-              onClick={() => navigate(-1)}
-              className=" hover:text-[#6556CD] ri-arrow-left-line cursor-pointer"
-            ></i>
-            Movie
-            <span className="text-lg ml-1 text-zinc-700">({movieData})</span>
-          </h1>
-          <div className=" w-[75%] text-white flex items-center justify-end ">
-            <div className="flex-1">
-              <TopNav />
+    useEffect(() => {
+        refreshHandler();
+    }, [movieData]);
+    return movie.length > 0 ? (
+        <>
+            <div className="w-full h-full">
+                <div className=" fixed top-0 bg-[#1F1E24] z-[100] w-full flex items-center justify-between px-[3%] ">
+                    <h1 className="text-2xl font-semibold text-zinc-600 w-[15%] ">
+                        <i
+                            onClick={() => navigate(-1)}
+                            className=" hover:text-[#6556CD] ri-arrow-left-line cursor-pointer"
+                        ></i>
+                        Movie
+                        <span className="text-lg ml-1 text-zinc-700">
+                            ({movieData})
+                        </span>
+                    </h1>
+                    <div className=" w-[75%] text-white flex items-center justify-end ">
+                        <div className="flex-1">
+                            <TopNav />
+                        </div>
+                        <DropDown
+                            title={"Category"}
+                            options={[
+                                "popular",
+                                "top_rated",
+                                "upcoming",
+                                "now_playing",
+                            ]}
+                            func={(e) => {
+                                setMovieData(e.target.value);
+                            }}
+                        />
+                    </div>
+                </div>
+
+                <InfiniteScroll
+                    dataLength={movie.length}
+                    next={getMoviePageData}
+                    hasMore={page}
+                    loader={<h4>Loading...</h4>}
+                >
+                    <Card data={movie} title="movie" />
+                </InfiniteScroll>
             </div>
-            <DropDown
-              title={"Category"}
-              options={["popular", "top_rated", "upcoming", "now_playing"]}
-              func={(e) => {
-                setMovieData(e.target.value);
-              }}
-            />
-          </div>
-        </div>
-
-        <InfiniteScroll
-          dataLength={movie.length}
-          next={getMoviePageData}
-          hasMore={page}
-          loader={<h4>Loading...</h4>}
-        >
-          <Card data={movie} title="movie" />
-        </InfiniteScroll>
-      </div>
-    </>
-  ) : (
-    <Loading />
-  );
+        </>
+    ) : (
+        <Loading />
+    );
 };
 
 export default Movie;
